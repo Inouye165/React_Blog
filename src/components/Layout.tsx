c9e17adb29df9e85fdef4d22eefd9bfc62436f02
inouye165 <inouye165@gmail.com>
2025-08-21 10:34:36 -0700

    fix(nav): point Journal link to /journal after hub change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <nav className="bg-white shadow flex items-center px-4 py-2">
-        <Link to="/" className="font-bold text-xl mr-4">Journal</Link>
+  <Link to="/journal" className="font-bold text-xl mr-4">Journal</Link>
         <Link to="/studies" className="mr-4">Studies</Link>
         <Link to="/post/new" className="mr-4">+ New Post</Link>
         <Link to="#" className="mr-4">Import/Export</Link>
